4b64a72d7374fcf4d5740555abe19651396c3220
victoria-sg <victoria-sg>
2026-07-07 12:17:22 -0500

    feat: pre-cargar consulta de ejemplo en apollo sandbox

--- servidor.js
+++ servidor.js
@@ -3,9 +3,32 @@ import { startStandaloneServer } from "@apollo/server/standalone";
 import { typeDefs } from "./esquema.js";
 import { resolvers } from "./resolvedores.js";
 
+import { ApolloServerPluginLandingPageLocalDefault } from "@apollo/server/plugin/landingPage/default";
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  plugins: [
+    ApolloServerPluginLandingPageLocalDefault({
+      embed: {
+        document: `query ObtenerDatosGimnasio {
+  miembros {
+    nombre
+    edad
+    plan {
+      nombre
+      precio
+    }
+  }
+  planes {
+    nombre
+    precio
+    duracionMeses
+  }
+}`
+      }
+    })
+  ]
 });
 
 const { url } = await startStandaloneServer(server, {
